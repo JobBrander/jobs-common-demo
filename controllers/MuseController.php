d35bb2a007e9b859a886f88caa40bdc2851f658d
Karl Hughes <khughes.me@gmail.com>
2015-12-20 15:35:27 -0500

    Finishing muse path

--- controllers/MuseController.php
+++ controllers/MuseController.php
@@ -1,5 +1,6 @@
 <?php namespace Controllers;
 
+use \JobBrander\Jobs\Client\Providers\Muse;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
@@ -8,10 +9,29 @@ class MuseController extends BaseController
     public function index(Request $request, Response $response)
     {
         $jobs = [];
+        $client = new Muse;
 
+        // Get the results from the API client
+        $results = $client->setCategory('Education')->getJobs();
+
+        // See all available fields by dumping the results variable
+        // var_dump($results->all());
+        
+        // Loop through, set up array of jobs for the table
+        foreach ($results->all() as $result) {
+            $jobs[] = [
+                'title' => $result->getTitle(),
+                'company' => $result->getCompanyName(),
+                'url' => $result->getUrl(),
+            ];
+        }
+
+        // Send variables out to the view
         return $this->container->view->render($response, 'provider.html', [
-            'name' => 'The Muse',
-            'jobs' => $jobs
+            'provider' => 'The Muse',
+            'code_path' => 'karllhughes/jobs-common-demo/blob/v2/controllers/MuseController.php',
+            'repository' => 'https://github.com/JobBrander/jobs-muse',
+            'results' => $jobs,
         ]);
     }
 }
